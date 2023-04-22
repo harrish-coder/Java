e27c080d07a167d2e65e38d52bac154aee63d965
Harrish <harrishoffical@gmail.com>
2023-04-22 22:13:39 +0530

    Add files via upload

---
+++ week 2/cli.java
@@ -0,0 +1,10 @@
+//Command line input in java
+
+class cli{
+    public static void main(String args[]) {
+        for(int i=0;i<args.length; i++){
+            System.out.println(args[i]+" ");
+        }
+        
+    }
+}
